f49ad1e7932b4eb302f6cec3e29209782219c92e
wased89 <braden.wright@yahoo.ca>
2013-11-15 16:37:26 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 using KsWind.Extensions;
 using KSP.IO;
@@ -10,8 +9,7 @@ namespace KsWind
     {
        
         private static Rect _windowPosition = new Rect();
-        public static float windSpeed = UnityEngine.Random.Range(0, 6)/ 10.0f;
-        public float TwindSpeed = windSpeed;
+        public float windSpeed = Random.Range(0, 6) / 10.0f;
         public bool inAtmo = true;
         public int atmoheight = 45000;
         public double vesselHeight = 0;
@@ -31,8 +29,9 @@ namespace KsWind
             }
             if (inAtmo == true)
             {
-                UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks; 
+                UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks;
                 windSpeed = UnityEngine.Random.Range(0, 6) / 10.0f;
+                
             }
             else
             {
@@ -60,8 +59,8 @@ namespace KsWind
         public override void OnUpdate()
         {
             double Pressure = FlightGlobals.getStaticPressure(FlightGlobals.ship_altitude);
-            UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks; 
-            if(this.vessel == FlightGlobals.ActiveVessel && windSpeedActive == true)
+            
+            if(this.vessel == FlightGlobals.ActiveVessel && Pressure != 0)
             {
 
                 this.rigidbody.AddForce(windSpeed,0,0);
@@ -72,19 +71,18 @@ namespace KsWind
         private void OnDraw()
         {
 
-            UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks; 
 
             vesselHeight = FlightGlobals.ship_altitude;
             
             if (Pressure != 0)
             {
-                UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks; 
+                windSpeed = UnityEngine.Random.Range(0, 6) / 10.0f;
                 windSpeedActive = true;
                 
             }
             else
             {
-                UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks; 
+                
                 windSpeedActive = false;
             }
             
@@ -102,7 +100,7 @@ namespace KsWind
 
         private void OnWindow(int _windowId)
         {
-            UnityEngine.Random.seed = (int)System.DateTime.Now.Ticks; 
+            
             double Pressure = FlightGlobals.getStaticPressure(FlightGlobals.ship_altitude);
             double HighestPressure = FlightGlobals.getStaticPressure(0.0);
             
@@ -112,7 +110,7 @@ namespace KsWind
             {
                 
                 GUILayout.BeginHorizontal(GUILayout.Width(600));
-                GUILayout.Label("windspeed: " + (windSpeed * 10) + " knots");
+                GUILayout.Label("windspeed: " + (windSpeed * 10) + " kernats");
                 GUILayout.Label("Vessel Altitude: " + vesselHeight);
                 GUILayout.Label("\rCurrent Atmoshperic Pressure: " + Pressure.ToString("0.000"));
                 GUILayout.Label("Highest Atmospheric Pressure: " + HighestPressure.ToString("0.000"));
@@ -122,8 +120,9 @@ namespace KsWind
             }
             else
             {
+                
                 GUILayout.BeginHorizontal(GUILayout.Width(600));
-                GUILayout.Label("windspeed: " + "0" + " knots");
+                GUILayout.Label("windspeed: " + "0" + " kernats");
                 GUILayout.Label("Vessel Altitude: " + vesselHeight);
                 GUILayout.Label("\rCurrent Atmoshperic Pressure: " + Pressure.ToString("0.000"));
                 GUILayout.Label("Highest Atmospheric Pressure: " + HighestPressure.ToString("0.000"));
